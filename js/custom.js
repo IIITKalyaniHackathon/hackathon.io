4469db69c9a93d703c5f7d1a19916ea16ded2a27
IIITKalyaniHackathon <iiitk.social@gmail.com>
2016-02-21 13:24:44 +0530

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
+
+$('.countdown').final_countdown({
+start: '1362139200',
+end: '1388461320',
+now: '1387461319'
+});
+
+
